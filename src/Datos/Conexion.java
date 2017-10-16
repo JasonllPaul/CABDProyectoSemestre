a8dfbe241eeb3624f7f6b1691ba255d149be5d55
Jason Paul Anturi Martínez <jasonpaulanma@hotmail.com>
2017-10-15 20:39:17 -0500

    Version 1.0

---
+++ src/Datos/Conexion.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Datos;
+
+
+import java.sql.*;
+
+/**
+ *
+ * @author JasonllPaul
+ */
+public class Conexion {
+    private String cadenaConexion;
+    private Connection con;
+    
+    private void inicializarConexion()
+    {
+        try
+        {
+          cadenaConexion = "jdbc:oracle:thin:@localhost:1521:xe";
+          con = DriverManager.getConnection(cadenaConexion, "system","jason");
+        
+        }
+        catch(Exception ex)
+        {
+        
+        }    
+    }
+    public Conexion()
+    {
+        inicializarConexion();
+    
+    }
+    
+    
+    /****************************************************************
+     * el método ejecutarDML recibe una consulta que fue enviada desde la capa de la logica.
+     * este metodo le permite ejecutar un insert, delete , update. al final este
+     * método devuelve un valor entero. 
+     * *****************************************************************/
+    public int ejecutarDML(String consulta)
+    {
+        int filasAfectadas = 0;
+        try
+        {
+            Statement st = con.createStatement();
+            filasAfectadas = st.executeUpdate(consulta);
+            return filasAfectadas;
+        }
+        catch(Exception ex)
+        {
+            return 0;     
+        
+        }    
+    
+    }
+    
+    /****************************************************************
+     * el método ejecutarSELECT recibe una consulta que fue enviada desde la capa de la logica.
+     * este metodo le permite ejecutar solo consultas SELECT. al final este
+     * método devuelve un conjunto de varios datos, un ResultSet.(recuerde que uns elect devulve varios 
+     * campos por eso todos esos campos se guardan en un ResultSet
+     * *****************************************************************/
+    public ResultSet ejecutarSELECT(String consulta)
+    {
+        ResultSet res;
+        try
+        {
+            Statement st = con.createStatement();
+            res = st.executeQuery(consulta);
+            return res;
+        }
+        catch(Exception ex)
+        {
+            return null;           
+        }    
+    
+    }
+    
+}
---
+++ src/Modelo/Ejemplo.java
@@ -0,0 +1,117 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import Datos.Conexion;
+import java.sql.ResultSet;
+import java.util.Locale;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ejemplo {
+    Conexion dt=new Conexion();
+
+    public int insertarDatos( int NIT, String NOMBRE, String DIRECCION, int FECHAFUNDACION ){
+        
+        String consulta;
+        int result;
+        consulta = "insert into EMPRESADISCOGRAFICA (NIT,NOMBRE,DIRECCION,FECHAFUNDACION) VALUES (%s, '%s', '%s', %s)";
+        consulta = String.format(consulta,NIT,NOMBRE,DIRECCION,FECHAFUNDACION);
+        //Envío la consulta a la capa de acceso a datos
+        result = dt.ejecutarDML(consulta);
+        return result;
+        
+    }
+    
+    public ResultSet consultarTodos(){
+    
+    String consulta="select * from EMPRESADISCOGRAFICA ORDER BY NIT ";
+    
+     ResultSet result = dt.ejecutarSELECT(consulta);
+    
+     return result;
+    }
+    
+    
+    public int actualizarRegistro(int n,String nom,String dir,int fecha){
+        String consulta="";
+
+        if(nom.length()>0 && dir.length()>0 && fecha>0){
+        
+        consulta="update EMPRESADISCOGRAFICA SET NOMBRE = '%s',DIRECCION = '%s',FECHAFUNDACION = %s where NIT = %s ";
+        
+        }
+        else{
+            
+            if(nom.length()>0 && dir.length()==0 && fecha>0){
+        
+            consulta="update EMPRESADISCOGRAFICA SET NOMBRE = '%s',FECHAFUNDACION = %s where NIT = %s ";
+            consulta = String.format(consulta,nom,fecha,n);
+            
+
+            }
+            if(nom.length()==0 && dir.length()>0 && fecha>0){
+        
+            consulta="update EMPRESADISCOGRAFICA SET DIRECCION = '%s',FECHAFUNDACION = %s where NIT = %s ";
+            consulta = String.format(consulta,dir,fecha,n);
+
+            }
+            
+            if(nom.length()==0 && dir.length()==0 && fecha>0){
+
+            consulta="update EMPRESADISCOGRAFICA SET FECHAFUNDACION = %s where NIT = %s ";
+            consulta = String.format(consulta,fecha,n);
+
+            }
+            
+            if(nom.length()==0 && dir.length()>0 && fecha==0){
+        
+            consulta="update EMPRESADISCOGRAFICA SET DIRECCION = '%s' where NIT = %s ";
+            consulta = String.format(consulta,dir,n);
+            }
+            
+            if( nom.length()>0 && dir.length()==0 && fecha==0){
+            consulta="update EMPRESADISCOGRAFICA SET NOMBRE = '%s' where NIT = %s ";
+            consulta = String.format(consulta,nom,n);
+            }
+            
+
+        }
+               
+        int result = dt.ejecutarDML(consulta);
+        
+        return result;
+    }
+    
+    
+    public ResultSet buscarRegistro(int nit){
+        
+        String consulta="select * from EMPRESADISCOGRAFICA where nit = %s";
+     consulta = String.format(consulta,nit);
+     ResultSet result = dt.ejecutarSELECT(consulta);
+    
+     return result;
+    }
+    
+    public ResultSet returnNit(){
+    
+        String consulta="select nit,nombre from empresadiscografica";
+        consulta=String.format(consulta);
+        ResultSet result =dt.ejecutarSELECT(consulta);
+        return result;
+    }
+    
+    public int comprobarExistencia(){
+        
+        String consulta="select nit from empresadiscografica";
+     consulta = String.format(consulta);
+     int result = dt.ejecutarDML(consulta);
+    
+     return result;
+    }
+    
+}
